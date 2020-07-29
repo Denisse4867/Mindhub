e8dd2fd36cfe8d2284ee7ac91a1a0216f6c05e8a
Melissa Pamela Juarez <melissapamela777@gmail.com>
2020-07-29 01:00:11 -0300

    news files js

---
+++ module-02/task-01/playground/arrays.js
@@ -0,0 +1,22 @@
+console.log (`starting javascript`);
+
+// Excercise 1:Arrays:
+//A:Create an array whit all the names of our class.
+var startNombres = [ `Katherine`, `Janneth`, `Nicole`, `Reyna`, `Melissa`, `Carolina`, `Macarena`, `Gabriela`, `Aylen`,
+					`Florencia`, `Luana`, `Malena`, `Yennyfer`,  `Verónica`,  `Micaela`,  `Azucena`,  `Belén`,  `Nadine`,
+					`Jhoelda`,  `Luz`, `Luz`,  `Florencia`,  `Catalina`,  `Florencia`,  `Belén`,  `Yael`, `Rocío`, `Magalí`,
+					`Camila`, `Andrea`, `Victoria`, `Carla`, `Rodrigo`, `Branko` ];
+					console.log(` Unsorted Array ${startNombres}`);
+
+//B:sort the array alphabetically.
+
+startNombres.sort();
+console.log(`Sorted Array ${startNombres}`);
+
+//C:Print the first element of the array in the console.
+console.log(`first index ${startNombres[0]}`);
+
+//D:Print the last element of the array in the console.
+
+
+//E: Use a "for" loop.
