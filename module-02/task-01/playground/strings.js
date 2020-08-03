a0d03d0581484d53786b55e508c56728316ff136
Melissa Pamela Juarez <melissapamela777@gmail.com>
2020-08-02 22:15:50 -0300

    strings.js added

--- module-02/task-01/playground/strings.js
+++ module-02/task-01/playground/strings.js
@@ -2,6 +2,28 @@ console.log (`starting javascript`);
 
 // Excercise 1:  Write a JavaScript function that reverses a number.
 
+function reverseString(input){
+	var reverse = [];
+	var array = input.split("")
+	for( var i = array.lenght; i>= 0 ; i--){
+		reverse.push(array[i])
+	}
+	return reverse.join("");
+}
+console.log(reverseString("ESCRIBIR")) //? funcion se puede poner con los simbolos y el backstick?
+console.log(reverseString())
+
+
+/* puede ser en reversa como: for(var i - array.lenght; i>0; i--).
+reverse.push(array[i]).
+return reverse.join ("");
+
+2 option:
+
+for( var i=0; i < array.lenght; i++){
+}
+return reverse;
+*/
 
 
 // Excercise 2:
